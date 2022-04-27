d271b4f93254e053589013b1a67f699c80f34196
Anuj dhakrey <anujdhakrey863@gmail.com>
2022-04-27 19:50:01 +0530

    stack_class_implemented

--- stack/stack_class.cpp
+++ stack/stack_class.cpp
@@ -1,47 +1,64 @@
-// Problem Link -
-#include<bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
-#define ll             long long int
-#define ld long double
-#define mod             1000000007
-#define inf             1e18
-#define endl            "\n"
-#define pb emplace_back
-#define vi              vector<ll>
-#define vs vector<string>
-#define pii             pair<ll,ll>
-#define ump unordered_map
-#define mp map
-#define pq_max          priority_queue<ll>
-#define pq_min          priority_queue<ll,vi,greater<ll> >
-#define ff first
-#define ss second
-#define mid(l,r)        (l+(r-l)/2)
-#define loop(i,a,b) for(int i=(a);i<=(b);i++)
-#define looprev(i,a,b) for(int i=(a);i>=(b);i--)
-#define log(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
-#define token(str,ch) (std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
-vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-cout << *it << " = " << a << endl;
-err(++it, args...);
+
+class myStack {
+    private:
+int peak =-1;
+int arr[10000];
+
+
+public:
+
+void push(int a){
+    if(peak!= 10000){
+        peak++;
+        arr[peak] = a;
+    }
+    else{
+        cout << "stack overflowed"<<endl;
+    }
 }
-void file_i_o(){
-ios_base::sync_with_stdio(0);
-cin.tie(0);
-cout.tie(0);
-#ifndef ONLINE_JUDG
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif
+
+void pop(){
+    if(peak!=-1){
+        peak--;
+    }
+    else{
+        cout << "stack is empty" << endl;
+    }
 }
-int main(int argc, char const *argv[]) {
-file_i_o();
-int n;
-cin >> n;
-cout << n;
-return 0;
+int top(){
+    if(peak == -1){
+        cout << "stack is empty" << endl;
+        return INT_MIN;
+    }
+    return arr[peak];
+}
+
+bool isempty(){
+    if(peak ==-1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+};
+
+int main(){
+
+    myStack obj1;
+    obj1.push(2);
+    obj1.push(3);
+    obj1.push(6);
+    obj1.push(9);
+    obj1.push(3);
+    int up = obj1.top();
+    obj1.pop();
+    up = obj1.top();
+    cout << up << endl;
+    obj1.peak = 10;
+    return 0;
 }
